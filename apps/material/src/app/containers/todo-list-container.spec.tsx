e1fc7226c93de8f7f5acc078c94dbccb1ee7badc
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2021-02-20 15:34:22 +0900

    chore: test suite

--- apps/material/src/app/containers/todo-list-container.spec.tsx
+++ apps/material/src/app/containers/todo-list-container.spec.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import { cleanup, render } from '@testing-library/react';
+import configureStore from "redux-mock-store"
 import TodoListContainer from './todo-list-container';
+const { Provider } = jest.requireActual("react-redux")
+const mockStore = configureStore()
+
+jest.mock("react-redux", () => ({
+  useselector: jest.fn().mockReturnValue({
+    todos: []
+  }),
+  useDispatch: jest.fn().mockReturnValue(jest.fn())
+}))
 
 describe('TodoListContainer', () => {
+  afterEach(() => {
+    cleanup()
+  })
   it('should render successfully', () => {
-    const { baseElement } = render(<TodoListContainer />);
+    const { baseElement } = render(
+      <Provider store={mockStore()}>
+        <TodoListContainer  />
+      </Provider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
